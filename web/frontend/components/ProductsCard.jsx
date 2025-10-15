f2aac8aa75c5489ef0fe17fd33c6dbddebd1e0a9
Sherif <sherifnabil441@gmail.com>
2025-10-15 12:17:31 +0300

    save and view custom meta field in component

--- web/frontend/components/ProductsCard.jsx
+++ web/frontend/components/ProductsCard.jsx
@@ -1,12 +1,26 @@
 import React, { useState, useEffect } from 'react';
-import { Page, Card, Form, TextField } from '@shopify/polaris';
+import { Page, Card, TextField } from '@shopify/polaris';
 import { useAppBridge, SaveBar } from '@shopify/app-bridge-react';
-// import {getSessionToken} from "@shopify/app-bridge-utils"; // For app instance
-
 export default function ProductsCard() {
     const [notes, setNotes] = useState('');
     const shopify = useAppBridge();
 
+    useEffect( () => {
+        const fetchData = async () => {
+            // const token = await getSessionToken(app);
+
+            const resp = await fetch("/api/custom/get", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            });
+
+            const json = await resp.json();
+            setNotes(json.metafield.value)
+        };
+        fetchData()
+    }, []);
     const handleSave = () => {
         save()
         shopify.saveBar.hide('my-save-bar');
@@ -16,20 +30,23 @@ export default function ProductsCard() {
         shopify.saveBar.show('my-save-bar')
     };
 
+
     const handleDiscard = () => {
         setNotes('')
         shopify.saveBar.hide('my-save-bar');
     };
 
+    // const host =
+    //     new URLSearchParams(location.search).get('host') ||
+    //     window.__SHOPIFY_DEV_HOST;
+
     const save = async () => {
-        const token = await window.shopify.idToken();
 
         try {
-            const resp = await fetch("http://127.0.0.1:8000/api/custom/save", {
+            const resp = await fetch("/api/custom/save", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
-                    // "Authorization": `Bearer ${token}`  ,
 
                 },
                 body: JSON.stringify({
@@ -40,28 +57,23 @@ export default function ProductsCard() {
 
             const json = await resp.json();
             if (!json.success) {
-                console.error("Save failed:", json.errors);
                 alert("Save failed");
             } else {
                 alert("Saved successfully");
             }
         } catch (err) {
-            console.log("Error saving:", err);
             alert("Error saving");
-        } finally {
         }
     };
 
     return (
         <Page title="Product Card">
             <Card sectioned>
-                <Form>
                     <TextField
                         label="Notes"
                         value={notes}
                         onChange={handleChange}
                     />
-                </Form>
             </Card>
             <SaveBar id="my-save-bar">
                 <button variant="primary" onClick={handleSave}></button>
